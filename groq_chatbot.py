d8720d1cf6f51e5896ce61fe6ea3c5ea5376ebfe
amreen <heedsites@gmail.com>
2026-01-22 07:21:01 -0800

    Update groq_chatbot.py

--- groq_chatbot.py
+++ groq_chatbot.py
@@ -8,7 +8,7 @@ class GroqChatbot:
     def get_response(self, prompt: str):
         chat = self.client.chat.completions.create(
             model=self.model,
-            messages=[{"role": "user", "content": prompt}],
+            messages=[{"role": "user", "content": prompt}], 
             temperature=0.7
         )
         return chat.choices[0].message.content
